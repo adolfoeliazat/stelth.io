ee3830f8333b1095e8b44f86e90420a71b6e394f
Alexandre Kim <alexandrekim93@gmail.com>
2017-03-29 15:52:38 -0700

    fixed get users for separation of concerns

--- server/api.js
+++ server/api.js
@@ -9,25 +9,7 @@ module.exports = (app) => {
         .skipUndefined()
         .where('users.id', req.query.id)
         .then(users => {
-            if(users.length === 0) {
-                User
-                .query()
-                .insertAndFetch({
-                    firstName: req.body.firstName,
-                    lastName: req.body.lastName,
-                    email: req.body.email,
-                    publicKey: req.body.publicKey
-                })
-                .then(user => {
-                    res.send(user)
-                })
-                .catch(err => {
-                    console.log("this shit is not working properly foo", err)
-                    next(err)
-                })
-            } else {
-                res.send(users)
-            }
+            res.send(users)
         })
         .catch(next);
     })
