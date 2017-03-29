a147bf2544469c836cc3977bd5f59abce26e1aa0
Alexandre Kim <alexandrekim93@gmail.com>
2017-03-29 10:09:42 -0700

    server start up

---
+++ server/server.js
@@ -0,0 +1,40 @@
+// Server starter-kit
+const express = require('express');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+
+// DB stuff
+const Model = require('objection');
+const Knex = require('knex');
+
+// Port application is running on
+const PORT = 3000;
+
+// Express initiation
+const app = express()
+    .use(bodyParser.json())
+    .use(bodyParser.urlencoded({
+        extended: true
+    }))
+    .use(morgan('dev'))
+    .use(express.static('/'));
+
+// Error handler
+app.use((err, req, res, next) => {
+    if(err) {
+        req.status(err.statusCode || err.status || 500).send(err.data || err.message || {});
+    } else {
+        next();
+    }
+});
+
+app.get('/', (res, req, next) => {
+    console.log("HELLO WORLD!");
+})
+
+// Server listener
+const server = app.listen(PORT, function() {
+    console.log(`Stelth listening on port ${PORT}`);
+});
+
+
