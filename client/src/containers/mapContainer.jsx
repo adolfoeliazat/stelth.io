926ec9b23813611ee1617844f2aa72b79455960f
Regina V. Lee <regina.v.lee@gmail.com>
2017-03-31 16:42:18 -0700

    markers rendering on the map

--- client/src/containers/mapContainer.jsx
+++ client/src/containers/mapContainer.jsx
@@ -22,21 +22,40 @@ class MapContainer extends React.Component {
       }
     })
     this.getDropLocations()
+    // this.renderDropMarkers()
   }
 
   getDropLocations() {
+    console.log('get drop locations')
     // axios call to db for drops then store in react state
-    console.log("am i here?")
+    // TODO: filter by users
     axios
       .get('http://localhost:3000/deadDrops')
       .then((result) => {
-        console.log(result)
+        this.setState({
+          markers: result.data
+        })
+      })
+      .then(() => {
+        this.renderDropMarkers()
       })
       .catch((err) => { console.log(err) })
   }
 
   renderDropMarkers() {
+    console.log('getting inside render drop markers: this.state.markers', this.state.markers)
     //get lat and lng from markers array in state and render
+    this.state.markers.forEach((drop) => {
+      let center = { 
+        lat: drop.lat, 
+        lng: drop.lng 
+      }
+      let marker = new google.maps.Marker({
+        position: center,
+        map: window.map
+      })
+      console.log('markers', marker);
+    })
   }
 
   render() {
