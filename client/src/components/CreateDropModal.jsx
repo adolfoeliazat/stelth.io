406d1953b2d8e17ded421b590a69323e055a05e7
Regina V. Lee <regina.v.lee@gmail.com>
2017-03-31 18:18:04 -0700

    set up multistep modal for file upload

--- client/src/components/CreateDropModal.jsx
+++ client/src/components/CreateDropModal.jsx
@@ -5,10 +5,33 @@ import { Modal, FormGroup, ControlLabel, FormControl, Button } from 'react-boots
 class CreateDropModal extends Component {
   constructor(props) {
     super(props);
-    // this.state = {
-    //   showModal: true
-    // }
+    this.state = {
+      step1: true,
+      step2: false,
+      complete: false
+    }
     this.close = this.close.bind(this)
+    this.goToNextStep = this.goToNextStep.bind(this)
+  }
+
+  goToNextStep() {
+    if (this.state.step1) {
+      this.setState({
+        step1: false,
+        step2: true,
+        complete: false
+      })
+    } else {
+      this.setState({
+        step1: false,
+        step2: false,
+        complete: true
+      })
+    }
+  }
+
+  handleInputchange() {
+
   }
 
   close() {
@@ -20,22 +43,48 @@ class CreateDropModal extends Component {
     return (
       <div>
         {console.log('testing')}
-        <Modal show={this.props.modalClicked} onHide={() => {this.props.toggleModal()}}>
+        <Modal show={this.props.modalClicked} onHide={() => { this.props.toggleModal() }}>
           <Modal.Header closeButton>Add a new Drop</Modal.Header>
           <Modal.Body>
-            <form>
-              <FormGroup>
-                <ControlLabel>Title</ControlLabel>
-                <FormControl
-                  name="formTitle"
-                  onChange={this.handleInputchange}
-                  componentClass="input"
-                />
-              </FormGroup>
-            </form>
+            {this.state.step1 ?
+              <form>
+                <FormGroup>
+                  <ControlLabel>Title</ControlLabel>
+                  <FormControl
+                    name="formTitle"
+                    onChange={this.handleInputchange}
+                    componentClass="input"
+                  />
+                </FormGroup>
+              </form> : ""}
+            {this.state.step2 ?
+              <form>
+                <FormGroup>
+                  <ControlLabel>Upload your files here:</ControlLabel>
+                  <FormControl
+                    name="formTitle"
+                    onChange={this.handleInputchange}
+                    componentClass="input"
+                  />
+                </FormGroup>
+              </form> : ""}
+            {this.state.complete ?
+              <form>
+                <FormGroup>
+                  <ControlLabel>Where would you like to place your drop?</ControlLabel>
+                  <FormControl
+                    name="formTitle"
+                    onChange={this.handleInputchange}
+                    componentClass="input"
+                  />
+                </FormGroup>
+              </form> : ""}
           </Modal.Body>
           <Modal.Footer>
-            <Button className="btn btn-primary" onClick={this.addUserItinerary}>Save</Button>
+            {this.state.complete ?
+              <Button className="btn btn-primary" onClick={() => { this.props.toggleModal() }}>Save</Button> :
+              <Button className="btn btn-primary" onClick={this.goToNextStep}>Next</Button>
+            }
           </Modal.Footer>
         </Modal>
       </div>
@@ -45,11 +94,3 @@ class CreateDropModal extends Component {
 
 export default CreateDropModal;
 
-
-
-          /*<Modal.Footer>
-            <button>CLOSE</button>
-          </Modal.Footer>*/
-          // {/*<Modal.Body >
-          //   {/*<p>What's going on?</p>*/}
-          // </Modal.Body>*/}
