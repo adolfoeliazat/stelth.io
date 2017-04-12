feb18c21651b6a16f9b6eebbfcd782aaa6598a1b
Armen Rostamian <armenr@gmail.com>
2017-04-12 14:52:57 -0700

    Emergency hotfix - server port fix

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ const knex = Knex(knexfile.development);
 Model.knex(knex);
 
 // Port application is running on
-const port = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
 console.log(path.join(__dirname, '/../client/build'))
 
