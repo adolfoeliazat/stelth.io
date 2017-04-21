1123398b231b2f39c08048a7b8ad80a54bc0714e
Armen Rostamian <armenr@gmail.com>
2017-04-21 15:08:27 -0700

    More text changes

--- client/src/components/AboutPage.jsx
+++ client/src/components/AboutPage.jsx
@@ -4,7 +4,7 @@ const AboutPage = () => (
   <div className="about-page">
     <div className="about-page-content">
       <h2>About STELTH.</h2>
-      <p>We are a team of three software engineers with a passion for innovative, cutting edge technology.</p>
+      <p>We are a passionate team of software engineers who like to get together and build cool things.</p>
       <p>Meet our team:</p>
     </div>
   </div>
